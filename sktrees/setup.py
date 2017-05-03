8eea31ea30ebf689cba3194eaa2e3a18814199e3
Raghav RV <rvraghav93@gmail.com>
2017-05-03 23:22:06 +0200

    Stripping it down a bit

--- sktrees/setup.py
+++ sktrees/setup.py
@@ -2,8 +2,6 @@ import os
 from os.path import join
 import warnings
 
-from sklearn._build_utils import maybe_cythonize_extensions
-
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
@@ -16,29 +14,14 @@ def configuration(parent_package='', top_path=None):
 
     config = Configuration('sktrees', parent_package, top_path)
 
-    # submodules with build utilities
-    config.add_subpackage('__check_build')
-    config.add_subpackage('_build_utils')
-
-    # submodules which have their own setup.py
-    # leave out "linear_model" and "utils" for now; add them after cblas below
     config.add_subpackage('ensemble')
     config.add_subpackage('tree')
-
-    # some libs needs cblas, fortran-compiled BLAS will not be sufficient
-    blas_info = get_info('blas_opt', 0)
-    if (not blas_info) or (
-            ('NO_ATLAS_INFO', 1) in blas_info.get('define_macros', [])):
-        config.add_library('cblas',
-                           sources=[join('src', 'cblas', '*.c')])
-        warnings.warn(BlasNotFoundError.__doc__)
 
     # add the test directory
     config.add_subpackage('tests')
 
-    maybe_cythonize_extensions(top_path, config)
-
     return config
+
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
