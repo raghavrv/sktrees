fa5d15933cf3142cefb428ae704c546e22c549ea
Raghav RV <rvraghav93@gmail.com>
2017-05-24 17:40:28 +0530

    Finally setup.py works

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 import os
 
-from setuptools import setup
+import setuptools
+from numpy.distutils.core import setup
+from numpy.distutils.misc_util import Configuration
 from setuptools import find_packages
+
 
 # The version will be the same as the scikit-learn version on which this can
 # be patched
@@ -16,7 +19,6 @@ def configuration(parent_package='', top_path=None):
     config = Configuration(None, parent_package, top_path)
 
     config.add_subpackage('sktrees')
-
     return config
 
 
@@ -45,8 +47,11 @@ def setup_package():
           version=VERSION,
           long_description=open('README.md').read(),
           install_requires=["scikit-learn==0.19.dev0"],
+          zip_safe=False,  # the package can run out of an .egg file
+          include_package_data=True,
           configuration=configuration
           )
+
 
 if __name__ == "__main__":
     setup_package()
